aff224423b216eb742e755f03b5ce4b369dc248d
DamienBitrise <60897705+DamienBitrise@users.noreply.github.com>
2022-07-26 14:49:11 -0700

    Update step.sh

--- step.sh
+++ step.sh
@@ -3,6 +3,25 @@
 set -ex
 
 echo "arch" > build.sh
-chmod 777 build.sh
-alias xcodebuild="arch -x86_64 /bin/bash ./build.sh"
+
+# Update .bashrc
+echo -e "alias xcodebuild=\"arch -x86_64 /bin/bash /Users/vagrant/git/build.sh\"\n" >> ~/.bashrc
+source ~/.bashrc &>/dev/null
+
+# Update .bash_profile
+#echo -e "shopt -s expand_aliases\n" >> ~/.bash_profile
+#echo -e ". ~/.bashrc\n" >> ~/.bash_profile
+#source ~/.bash_profile &>/dev/null
+
+# Update .zshrc
+echo -e "shopt -s expand_aliases\n" >> ~/.zshrc
+echo -e ". ~/.bashrc\n" >> ~/.zshrc
+source ~/.zshrc &>/dev/null
+
+echo "Running arch"
+arch
+
+echo ""
+
+echo "Running Swizzled xcodebuild"
 xcodebuild
